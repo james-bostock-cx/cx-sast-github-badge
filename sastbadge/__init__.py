ad53861b7a746b1e0f15dc1612e85e7801a6c3b0
James Bostock <James.Bostock@checkmarx.com>
2024-10-16 11:24:24 +1100

    Use the logging module instead of print()

--- sastbadge/__init__.py
+++ sastbadge/__init__.py
@@ -1,9 +1,11 @@
+import logging
+import os
 from flask import Flask
 from CheckmarxPythonSDK.CxRestAPISDK import ScansAPI
 
 SCAN_STATUS_FINISHED = 7
 
-def generate_badge(project_id, scans_api=None):
+def generate_badge(project_id, logger, scans_api=None):
 
     if not scans_api:
         scans_api = ScansAPI()
@@ -16,14 +18,16 @@ def generate_badge(project_id, scans_api=None):
 
     scans = scans_api.get_all_scans_for_project(project_id=project_id, last=1)
     if len(scans) != 1:
+        logger.debug(f'No scans found for project {project_id}')
         badge['message'] = 'Unscanned'
         badge['color'] = 'Red'
         return badge
 
     scan = scans[0]
+    logger.debug(f'Scan status: {scan.status}')
     if scan.status.id == SCAN_STATUS_FINISHED:
         stats = scans_api.get_statistics_results_by_scan_id(scan.id)
-        print(f'stats: {stats}')
+        logger.debug(f'Scan statistics: {stats}')
         if (stats.high_severity == 0 and
             stats.medium_severity == 0 and
             stats.low_severity == 0 and
@@ -37,7 +41,7 @@ def generate_badge(project_id, scans_api=None):
         badge['message'] = 'Scan not finished'
         badge['color'] = 'orange'
 
-    print(f'badge: {badge}')
+    logger.debug(f'Badge: {badge}')
     return badge
 
 
@@ -45,10 +49,15 @@ def create_app(test_config=None):
 
     app = Flask(__name__)
 
+    log_level = os.environ.get('SAST_BADGE_LOG_LEVEL', 'WARN')
+    app.logger.setLevel(log_level)
+    # Without the following we get duplicate log messages
+    app.logger.propagate = False
+
     @app.route('/badge')
     def badge():
         try:
-            return generate_badge(34)
+            return generate_badge(34, app.logger)
         except Exception as e:
             print(e)
             return {
